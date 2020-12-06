8e0609a0331682fea20dbaac38a0c16f608c43bb
Samer Buna <samer@jscomplete.com>
2020-12-06 11:07:59 -0800

    9.8

--- web/src/components/MyTasks.js
+++ web/src/components/MyTasks.js
@@ -1,27 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
 import { useStore } from '../store';
-import TaskSummary from './TaskSummary';
+import TaskSummary, { TASK_SUMMARY_FRAGMENT } from './TaskSummary';
 
-/** GIA NOTES
- * Define GraphQL operations here...
- */
+const MY_TASK_LIST = `
+  query myTaskList {
+    me {
+      taskList {
+        id
+        ...TaskSummary
+      }
+    }
+  }
+  ${TASK_SUMMARY_FRAGMENT}
+`;
 
 export default function MyTasks() {
   const { request } = useStore();
   const [myTaskList, setMyTaskList] = useState(null);
 
   useEffect(() => {
-    /** GIA NOTES
-     *
-     *  1) Invoke the query to get current user list of Tasks
-     *     (You can't use `await` here but `promise.then` is okay)
-     *
-     *  2) Use the line below on the returned data:
-
-      setMyTaskList(API_RESP_FOR_userTaskList);
-
-     */
+    request(MY_TASK_LIST).then(({ data }) => {
+      setMyTaskList(data.me.taskList);
+    });
   }, [request]);
 
   if (!myTaskList) {
