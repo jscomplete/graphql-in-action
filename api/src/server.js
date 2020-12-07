2e11d80fbb0318d0d7c0be72df60516a3a5d9df4
Samer Buna <samer@jscomplete.com>
2020-12-06 19:26:13 -0800

    10.9

--- api/src/server.js
+++ api/src/server.js
@@ -1,93 +1,75 @@
+import DataLoader from 'dataloader';
 import { ApolloServer } from 'apollo-server';
-import { graphqlHTTP } from 'express-graphql';
+
 import { schema } from './schema';
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import morgan from 'morgan';
-
-import * as config from './config';
-
 import pgApiWrapper from './db/pg-api';
 import mongoApiWrapper from './db/mongo-api';
 
-import DataLoader from 'dataloader';
+import * as config from './config';
 
 async function main() {
   const pgApi = await pgApiWrapper();
   const mongoApi = await mongoApiWrapper();
-  const server = express();
-  server.use(cors());
-  server.use(morgan('dev'));
-  server.use(bodyParser.urlencoded({ extended: false }));
-  server.use(bodyParser.json());
-  server.use('/:fav.ico', (req, res) => res.sendStatus(204));
 
-  server.use('/', async (req, res) => {
-    const authToken =
-      req && req.headers && req.headers.authorization
-        ? req.headers.authorization.slice(7) // "Bearer "
-        : null;
-    const currentUser = await pgApi.userFromAuthToken(authToken);
-    if (authToken && !currentUser) {
-      return res.status(401).send({
-        errors: [{ message: 'Invalid access token' }],
-      });
-    }
-    const loaders = {
-      users: new DataLoader((userIds) => pgApi.usersInfo(userIds)),
-      approachLists: new DataLoader((taskIds) =>
-        pgApi.approachLists(taskIds)
-      ),
-      tasks: new DataLoader((taskIds) =>
-        pgApi.tasksInfo({ taskIds, currentUser })
-      ),
-      tasksByTypes: new DataLoader((types) =>
-        pgApi.tasksByTypes(types)
-      ),
-      searchResults: new DataLoader((searchTerms) =>
-        pgApi.searchResults({ searchTerms, currentUser })
-      ),
-      detailLists: new DataLoader((approachIds) =>
-        mongoApi.detailLists(approachIds)
-      ),
-      tasksForUsers: new DataLoader((userIds) =>
-        pgApi.tasksForUsers(userIds)
-      ),
-    };
-    const mutators = {
-      ...pgApi.mutators,
-      ...mongoApi.mutators,
-    };
-    graphqlHTTP({
-      schema,
-      context: { loaders, mutators, currentUser },
-      graphiql: { headerEditorEnabled: true },
-      customFormatErrorFn: (err) => {
-        const errorReport = {
-          message: err.message,
-          locations: err.locations,
-          stack: err.stack ? err.stack.split('\n') : [],
-          path: err.path,
-        };
-        console.error('GraphQL Error', errorReport);
-        return config.isDev
-          ? errorReport
-          : { message: 'Oops! Something went wrong! :(' };
-      },
-    })(req, res);
+  const server = new ApolloServer({
+    schema,
+    formatError: (err) => {
+      const errorReport = {
+        message: err.message,
+        locations: err.locations,
+        stack: err.stack ? err.stack.split('\n') : [],
+        path: err.path,
+      };
+      console.error('GraphQL Error', errorReport);
+      return config.isDev
+        ? errorReport
+        : { message: 'Oops! Something went wrong! :(' };
+    },
+    context: async ({ req }) => {
+      const authToken =
+        req && req.headers && req.headers.authorization
+          ? req.headers.authorization.slice(7) // "Bearer "
+          : null;
+      const currentUser = await pgApi.userFromAuthToken(authToken);
+      if (authToken && !currentUser) {
+        throw Error('Invalid access token');
+      }
+      const loaders = {
+        users: new DataLoader((userIds) => pgApi.usersInfo(userIds)),
+        approachLists: new DataLoader((taskIds) =>
+          pgApi.approachLists(taskIds)
+        ),
+        tasks: new DataLoader((taskIds) =>
+          pgApi.tasksInfo({ taskIds, currentUser })
+        ),
+        tasksByTypes: new DataLoader((types) =>
+          pgApi.tasksByTypes(types)
+        ),
+        searchResults: new DataLoader((searchTerms) =>
+          pgApi.searchResults({ searchTerms, currentUser })
+        ),
+        detailLists: new DataLoader((approachIds) =>
+          mongoApi.detailLists(approachIds)
+        ),
+        tasksForUsers: new DataLoader((userIds) =>
+          pgApi.tasksForUsers(userIds)
+        ),
+      };
+      const mutators = {
+        ...pgApi.mutators,
+        ...mongoApi.mutators,
+      };
+
+      return { loaders, mutators, currentUser };
+    },
   });
 
-  server.listen(config.port, () => {
-    console.log(`Server URL: http://localhost:${config.port}/`);
-  });
-
-  const serverWS = new ApolloServer({ schema });
-
-  serverWS.listen({ port: 4000 }).then(({ subscriptionsUrl }) => {
-    console.log(`Subscriptions URL: ${subscriptionsUrl}`);
-  });
+  server
+    .listen({ port: config.port })
+    .then(({ url, subscriptionsUrl }) => {
+      console.log(`Server URL: ${url}`);
+      console.log(`Subscriptions URL: ${subscriptionsUrl}`);
+    });
 }
 
 main();
